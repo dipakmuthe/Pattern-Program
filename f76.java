6500e8599036a108a4a54f11d51b9037903b006e
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-07-28 10:39:15 +0530

    Add files via upload

---
+++ f76.java
@@ -0,0 +1,54 @@
+package Loops;
+
+public class f76 {
+
+	public static void main(String[] args) {
+		// Butterfly Pattern
+		int n=5;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+				
+			}
+			for(int j=i;j<n;j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=i;j<n;j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+		
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			for(int j=i;j<n;j++)
+			{
+				System.out.print("  ");
+			}
+			for(int j=i;j<n;j++)
+			{
+				System.out.print("  ");
+			}
+			
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
