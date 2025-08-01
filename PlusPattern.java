4d4ef4d0c4ed2aec7a25ee77c725dd3bcec2d944
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-08-01 10:26:52 +0530

    Create PlusPattern.java

---
+++ PlusPattern.java
@@ -0,0 +1,26 @@
+package Loops;
+
+public class PlusPattern{
+//plus sign pattern using stars.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=10;
+		int a=n/2;
+		for(int i=1;i<n;i++)
+		{
+			for(int j=1;j<n;j++ )
+			{
+				if(j==a || i==a )
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
