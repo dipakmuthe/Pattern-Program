e86c030ad5e904f10e5271c0939fcb9026eaf8b9
DIPAK Muthe <129575863+dipakmuthe@users.noreply.github.com>
2025-08-16 11:33:35 +0530

    Add files via upload

---
+++ PalindromPyaramidNumbers.java
@@ -0,0 +1,28 @@
+package NumberPattern;
+import java.util.*;
+public class PalindromPyaramidNumbers {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter limit::");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print("  ");
+			}
+			for(int k=i;k>=1;k--)
+			{
+				System.out.print(k+" ");
+			}
+			for(int k=2;k<=i;k++)
+			{
+				System.out.print(k+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
